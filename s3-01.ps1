ab2315cffdcf8d89b033a3b2c1d2846e17a87291
Karol <93782725+petitess@users.noreply.github.com>
2023-10-15 13:52:06 +0200

    upload/download files/folders

--- s3-01.ps1
+++ s3-01.ps1
@@ -6,3 +6,5 @@ aws s3 cp C:\Users\SEK\Pictures\reolink s3://s3-infra-test-01/reolink --recursiv
 aws s3 cp s3://s3-infra-test-01/A.png C:\Users\SEK\Pictures\reolink2\A.png
 #download folder
 aws s3 cp s3://s3-infra-test-01/reolink C:\Users\SEK\Pictures\reolink2 --recursive
+#remove all objects
+aws s3 rm s3://s3-infra-test-01 --recursive
