c814257a23376fbe94a45b8e2332a70c9a557550
Karol <93782725+petitess@users.noreply.github.com>
2023-10-15 13:45:57 +0200

    upload/download files/folders

---
+++ s3-01.ps1
@@ -0,0 +1,8 @@
+#upload file
+aws s3 cp C:\Users\SEK\Pictures\A.png s3://s3-infra-test-01
+#upload folder
+aws s3 cp C:\Users\SEK\Pictures\reolink s3://s3-infra-test-01/reolink --recursive
+#download file
+aws s3 cp s3://s3-infra-test-01/A.png C:\Users\SEK\Pictures\reolink2\A.png
+#download folder
+aws s3 cp s3://s3-infra-test-01/reolink C:\Users\SEK\Pictures\reolink2 --recursive
